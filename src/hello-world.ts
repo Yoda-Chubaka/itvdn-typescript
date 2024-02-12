71d29ea934d789011a9c8d15334de902a5de7bbc
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-02-12 15:33:40 +0200

    Update

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -66,9 +66,9 @@ switch (marshmallow) {
         price3 = 0;
         break;
 }
-    orderSize();
-    orderTopping();
-    orderMarshmallow();
+    // orderSize();
+    // orderTopping();
+    // orderMarshmallow();
 var total: number = price1 + price2 + price3;
 document.body.appendChild(domElement);
 return total;
@@ -77,20 +77,20 @@ return total;
 var result = iceCream(Size.Large, Topping.Caramel, Marshmallow.Yes);
 console.log(result);
 
-function orderSize() {
-    var message:string;
-    message = prompt("Enter size");
-    document.getElementById("demo1").innerHTML = message;
-}
+// function orderSize() {
+//     var message:string;
+//     message = prompt("Enter size");
+//     document.getElementById("demo1").innerHTML = message;
+// }
 
-function orderTopping() {
-    var message:string;
-    message = prompt("Enter topping");
-    document.getElementById("demo2").innerHTML = message;
-}
+// function orderTopping() {
+//     var message:string;
+//     message = prompt("Enter topping");
+//     document.getElementById("demo2").innerHTML = message;
+// }
 
-function orderMarshmallow() {
-    var message:string;
-    message = prompt("Enter marshmellow (if you want)");
-    document.getElementById("demo3").innerHTML = message;
-}
+// function orderMarshmallow() {
+//     var message:string;
+//     message = prompt("Enter marshmellow (if you want)");
+//     document.getElementById("demo3").innerHTML = message;
+// }

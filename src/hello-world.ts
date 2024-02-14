585ea910a30ba7e7566aa84e645b8be8f364d58c
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-02-14 10:14:47 +0200

    Added abstact class

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -12,70 +12,76 @@
 
 // var3();
 
-enum Size {
-    Small, 
-    Large
-}
 
-enum Topping {
-    Chocolate,
-    Caramel,
-    Berries
-}
 
-enum Marshmallow {
-    Yes,
-    No
-}
+// ICE CREAM PRICE
+// enum Size {
+//     Small, 
+//     Large
+// }
 
-function iceCream(size: Size, topping: Topping, marshmallow?: Marshmallow) {
-    const domElement: HTMLDivElement = document.createElement("div");
+// enum Topping {
+//     Chocolate,
+//     Caramel,
+//     Berries
+// }
+
+// enum Marshmallow {
+//     Yes,
+//     No
+// }
+
+// function iceCream(size: Size, topping: Topping, marshmallow?: Marshmallow) {
+//     const domElement: HTMLDivElement = document.createElement("div");
     
 
-var price1:number;
-var price2:number;
-var price3:number;
-switch (size) {
-    case Size.Small:
-        price1 = 10;
-        break;
+// var price1:number;
+// var price2:number;
+// var price3:number;
+// switch (size) {
+//     case Size.Small:
+//         price1 = 10;
+//         break;
 
-    case Size.Large:
-        price1 = 25;
-        break;
-}
-switch (topping) {
-    case Topping.Chocolate:
-        price2 = 5;
-        break;
+//     case Size.Large:
+//         price1 = 25;
+//         break;
+// }
+// switch (topping) {
+//     case Topping.Chocolate:
+//         price2 = 5;
+//         break;
 
-    case Topping.Caramel:
-        price2 = 6;
-        break;
+//     case Topping.Caramel:
+//         price2 = 6;
+//         break;
 
-    case Topping.Berries:
-        price2 = 10;
-        break;
-}
-switch (marshmallow) {
-    case Marshmallow.Yes:
-        price3 = 5;
-        break;
+//     case Topping.Berries:
+//         price2 = 10;
+//         break;
+// }
+// switch (marshmallow) {
+//     case Marshmallow.Yes:
+//         price3 = 5;
+//         break;
 
-    case Marshmallow.No:
-        price3 = 0;
-        break;
-}
-    // orderSize();
-    // orderTopping();
-    // orderMarshmallow();
-var total: number = price1 + price2 + price3;
-document.body.appendChild(domElement);
-return total;
-}
+//     case Marshmallow.No:
+//         price3 = 0;
+//         break;
+// }
+//     // orderSize();
+//     // orderTopping();
+//     // orderMarshmallow();
+// var total: number = price1 + price2 + price3;
+// document.body.appendChild(domElement);
+// return total;
+// }
 
-var result = iceCream(Size.Large, Topping.Caramel, Marshmallow.Yes);
-console.log(result);
+// var result = iceCream(Size.Large, Topping.Caramel, Marshmallow.Yes);
+// console.log(result);
+
+
+
 
 // function orderSize() {
 //     var message:string;
@@ -94,3 +100,16 @@ console.log(result);
 //     message = prompt("Enter marshmellow (if you want)");
 //     document.getElementById("demo3").innerHTML = message;
 // }
+
+
+
+// ABSRTACT CLASSES
+abstract class Car {
+    constructor (public name: string) {}
+
+    abstract makeSound(): void;
+
+    public move(): void {
+        console.log(this.name + " підвозить з вітерцем");
+    }
+}

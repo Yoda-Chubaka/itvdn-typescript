32ce885a75c464d85d4a30d5aae453169fbfb368
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2024-02-14 11:02:51 +0200

    Added interface

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -104,60 +104,76 @@
 
 
 // ABSRTACT CLASSES
-abstract class Car {
-    constructor (public name: string) {}
+// abstract class Car {
+//     constructor (public name: string) {}
 
-    abstract makeSound(): void;
+//     abstract makeSound(): void;
 
-    public move(): void {
-        console.log(this.name + " підвозить з вітерцем");
-    }
+//     public move(): void {
+//         console.log(this.name + " підвозить з вітерцем");
+//     }
+// }
+
+// class Audi extends Car {
+//     constructor(protected price: number, private owner: string) {
+//         super("Ауді");
+//         console.log("Ауді коштує " + this.price);
+//         console.log("Власник " + this.owner);
+//     }
+
+//     makeSound(): void {
+//         console.log("Бі-біп!!");
+//     }
+// }
+
+// class BMW extends Car {
+//     constructor(protected price: number, private owner: string) {
+//         super("БМВ");
+//         console.log("БМВ коштує " + this.price);
+//         console.log("Власник " + this.owner);
+//     }
+
+//     makeSound(): void {
+//         console.log("Ввжух!!");
+//     }
+// }
+
+// class Tavria extends Car {
+//     constructor(protected price: number, private owner: string) {
+//         super("Таврія не");
+//         console.log("Таврія коштує " + this.price);
+//         console.log("Власник " + this.owner);
+//     }
+
+//     makeSound(): void {
+//         console.log("Ніяк не звучить, бо таврія не заводиться");
+//     }
+// }
+
+// const audi: Car = new Audi(3000, "Герр фон Волф");
+// audi.move();
+// audi.makeSound();
+
+// const bmv: Car = new BMW(4000, "Фрау Ельза");
+// bmv.move();
+// bmv.makeSound();
+
+// const tavria: Car = new Tavria(200, "Дід Микола");
+// tavria.move();
+// tavria.makeSound();
+
+
+// INTERFACES
+interface AnimalDescription {
+    name?: string;
+    nutrition: string;
+    habitat: string;
 }
 
-class Audi extends Car {
-    constructor(protected price: number, private owner: string) {
-        super("Ауді");
-        console.log("Ауді коштує " + this.price);
-        console.log("Власник " + this.owner);
+function drawAnimal(animal: AnimalDescription): void {
+    if (animal.name) {
+        console.log("Name is " + animal.name);
     }
 
-    makeSound(): void {
-        console.log("Бі-біп!!");
-    }
+    console.log("It eats " + animal.nutrition + " and lives in " + animal.habitat);
 }
-
-class BMW extends Car {
-    constructor(protected price: number, private owner: string) {
-        super("БМВ");
-        console.log("БМВ коштує " + this.price);
-        console.log("Власник " + this.owner);
-    }
-
-    makeSound(): void {
-        console.log("Ввжух!!");
-    }
-}
-
-class Tavria extends Car {
-    constructor(protected price: number, private owner: string) {
-        super("Таврія не");
-        console.log("Таврія коштує " + this.price);
-        console.log("Власник " + this.owner);
-    }
-
-    makeSound(): void {
-        console.log("Ніяк не звучить, бо таврія не заводиться");
-    }
-}
-
-const audi: Car = new Audi(3000, "Герр фон Волф");
-audi.move();
-audi.makeSound();
-
-const bmv: Car = new BMW(4000, "Фрау Ельза");
-bmv.move();
-bmv.makeSound();
-
-const tavria: Car = new Tavria(200, "Дід Микола");
-tavria.move();
-tavria.makeSound();
